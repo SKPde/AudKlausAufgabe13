0e6871d6e1a59ad4ef2fb2dfdba691fc54ec2b17
s.kuebel <s.kuebel@otterbach.de>
2017-08-15 11:51:28 +0200

    Unimplementierte Methoden hinzugefügt

---
+++ src/ReelleZahl.java
@@ -0,0 +1,29 @@
+
+public class ReelleZahl extends Zahl implements Comparable{
+	
+	private double value;
+	
+	public ReelleZahl(double v) {
+		value = v;
+	}
+
+	@Override
+	public Zahl add(Zahl Zahl) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public boolean equals(Object zahl) {
+		
+		
+		
+		return false;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
